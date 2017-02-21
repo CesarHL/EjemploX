fb15887056ed7f6f792f2a30a8780165af71745e
CesarHL <cesar.herlop@yahoo.com.mx>
2017-02-21 01:48:29 -0600

    cambiando a db.properties

--- src/java/com/ipn/escom/util/Util.java
+++ src/java/com/ipn/escom/util/Util.java
@@ -1,30 +1,42 @@
 package com.ipn.escom.util;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
 
-    private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
-    private static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/userdb";
-    private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "root";
+    private static Connection connection = null;
 
     public static Connection getConnection() {
-
-        Connection dbConnection = null;
-
-        try {
-
-            Class.forName(DB_DRIVER);
-            dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
-
-            return dbConnection;
-
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println(e.getMessage());
+        if (connection != null) {
+            return connection;
+        } else {
+            try {
+                Properties prop = new Properties();
+                InputStream inputStream = Util.class.getClassLoader().getResourceAsStream("/db.properties");
+                prop.load(inputStream);
+                String driver = prop.getProperty("driver");
+                String url = prop.getProperty("url");
+                String user = prop.getProperty("user");
+                String password = prop.getProperty("password");
+                Class.forName(driver);
+                connection = DriverManager.getConnection(url, user, password);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return connection;
         }
-        return dbConnection;
+
     }
 }
